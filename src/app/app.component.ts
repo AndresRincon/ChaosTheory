0b885eba249f9f8eeb3b0fc3623c2a8670a8025c
PaulaCarrasco <paula-vanessa@misena.edu.co>
2017-09-22 00:31:16 -0500

    pruebaCommit
    Segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}} + Andres</h1>`,
+  template: `<h1>Hello {{name}} + Chaos Theory</h1>`,
 })
 export class AppComponent  { name = 'Angular'; }
